b90623736d77eefa4e220ebb4a3f96c5eb5132fa
kow <kow@em70.ru>
2017-06-17 23:23:45 +0700

    Project init

---
+++ PhotoServer/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace PhotoServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.UserName = "test";
+            factory.Password = "test";
+            factory.VirtualHost = "/";
+            factory.Protocol = Protocols.DefaultProtocol;
+            factory.HostName = "192.168.0.40";
+            factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+
+
+        EventingBasicConsumer consumer;
+        private void test()
+        {
+            var factory = new ConnectionFactory();
+            factory.UserName = "test";
+            factory.Password = "test";
+            factory.VirtualHost = "/";
+            factory.Protocol = Protocols.DefaultProtocol;
+            factory.HostName = "192.168.0.40";
+            factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     noAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            test();
+        }
+    }
+}
---
+++ PhotoServer/Global/PhotoShoot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoServer.Global
+{
+    public class PhotoShoot
+    {
+        private int stend;
+        private int cameraId;
+        private int count;
+        private int delay;
+        private string photoId;
+
+        public int Stend
+        {
+            get { return stend; }
+            set { stend = value; }
+        }
+
+        public int CameraId
+        {
+            get { return cameraId; }
+            set { cameraId = value; }
+        }
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        public int Delay
+        {
+            get { return delay; }
+            set { delay = value;}
+        }
+
+        public string PhotoId
+        {
+            get { return photoId; }
+            set { photoId = value; }
+        }
+    }
+}
